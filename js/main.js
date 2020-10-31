2b2f5c296fbe316f2a76cf4c52b127650dbda3bf
eslamkhaled201 <59029234+eslamkhaled201@users.noreply.github.com>
2020-10-31 09:49:40 +0200

    update css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 let mainHeading = document.querySelector(".typewritter");
-
-$('main').scrollspy({ target: '#navbar' })
+var navBar = document.querySelector('.navbar');
+function editNavBar() {
+    if ($(window).scrollTop() >= 400) {
+      navBar.classList.replace('navbar-dark', 'navbar-light');
+      $('.navbar').addClass("bg-light navbar-shadow ");
+    } else {
+      navBar.classList.replace('navbar-light', 'navbar-dark');
+      $('.navbar').removeClass("bg-light navbar-shadow ");
+    }
+}
+$(window).scroll(function (){
+    editNavBar();
+});
+$('main').scrollspy({ target: '#navbar' });
 let typewritter = new Typewriter( mainHeading ,{
     strings:['Developer', 'Designer', 'Larry Daniels' ],
     loop:true,
@@ -9,3 +21,5 @@ let typewritter = new Typewriter( mainHeading ,{
     typeSpeed:60,
     deleteSpeed:60
 })
+
+
