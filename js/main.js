1930971cd45db3f81c8b1124ce6ec8183a06bb9f
eslamkhaled201 <59029234+eslamkhaled201@users.noreply.github.com>
2020-11-04 17:49:50 +0200

    edit project

--- js/main.js
+++ js/main.js
@@ -1,25 +1,45 @@
 let mainHeading = document.querySelector(".typewritter");
-var navBar = document.querySelector('.navbar');
+let navBar = document.querySelector('.navbar');
+let counters = document.querySelectorAll('.number');
+let speed = 200;
+
 function editNavBar() {
-    if ($(window).scrollTop() >= 400) {
-      navBar.classList.replace('navbar-dark', 'navbar-light');
-      $('.navbar').addClass("bg-light navbar-shadow ");
+    if (window.scrollY >= 400) {
+        navBar.classList.replace('navbar-dark', 'navbar-light');
+        $(navBar).addClass("bg-light navbar-shadow");
     } else {
-      navBar.classList.replace('navbar-light', 'navbar-dark');
-      $('.navbar').removeClass("bg-light navbar-shadow ");
+        navBar.classList.replace('navbar-light', 'navbar-dark');
+        $(navBar).removeClass("bg-light navbar-shadow");
     }
 }
-$(window).scroll(function (){
+
+$(window).scroll(function () {
     editNavBar();
+    if (window.scrollY >= 3100) {
+        counters.forEach(counter => {
+            let updateCount = () => {
+                let target = +counter.getAttribute('data-target');
+                let currentCount = +counter.innerHTML;
+                let incValue = target / speed;
+                if (currentCount < target) {
+                    counter.innerHTML = Math.ceil(currentCount + incValue);
+                    setTimeout(updateCount, 1);
+                } else {
+                    counter.innerHTML = target;
+                }
+                
+            }
+            updateCount();
+        });
+    }
 });
 $('main').scrollspy({ target: '#navbar' });
-let typewritter = new Typewriter( mainHeading ,{
-    strings:['Developer', 'Designer', 'Larry Daniels' ],
-    loop:true,
-    autoStart:true,
+let typewritter = new Typewriter(mainHeading, {
+    strings: ['Developer', 'Designer', 'Larry Daniels'],
+    loop: true,
+    autoStart: true,
     cursur: "Pipe character",
-    typeSpeed:60,
-    deleteSpeed:60
+    typeSpeed: 60,
+    deleteSpeed: 60
 })
-
 
